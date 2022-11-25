eefb804259cb1754cd9f847f0a708bb03db3fd9f
Rilakkumari <kubotamarlene@gmail.com>
2022-11-25 14:41:42 +0900

    added photos and more CSS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001b437c29aafeb4375a9883710d8eb7ced";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -26,31 +38,33 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function load(){
+  function load() {
     setLoaded(true);
     search();
   }
 
-  if(loaded){
+  if (loaded) {
     return (
-    <div className="Dictionary">
-      <section>
-        <h1>What are you looking for?</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
-        </form>
-        <div className="hint">
-          suggested words: sunset, wine, forest, yoga, etc.
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
-} else {
+      <div className="Dictionary">
+        <section>
+          <h1>What are you looking for?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            i.e. sunset, wine, forest, yoga, etc.
+          </div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
     load();
-return "Loading..";
-}
-
-
-    
+    return "Loading..";
+  }
 }
